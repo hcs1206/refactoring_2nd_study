dea62dac66057208b27f844844e3140383d6e0e6
changseop_hwang@tmax.co.kr <changseop_hwang@tmax.co.kr>
2022-04-06 15:12:19 +0900

    1.6 중첩 함수 분리 HTML render 버전 추가

--- ch1_refactoring_first_example/statement.js
+++ ch1_refactoring_first_example/statement.js
@@ -11,9 +11,29 @@ function renderPlainText(data){
         result +=` ${performance.play.name}: ${usd(performance.amount)} (${performance.audience}석)\n`;
     }
     
-    // 청구 내역을 출력한다
     result += `총액: ${usd(data.totalAmount)}\n`
     result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+
+    return result;   
+}
+
+function htmlStatement(invoice, plays){
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data){
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th><tr>"
+    
+    for(let performance of data.performances){
+        result +=` <tr><td>${performance.play.name}</td><td>(${performance.audience}석)</td>`;
+        result +=`<td>${usd(performance.amount)}</td></tr>\n`
+    }
+    
+    result += "</table>\n"
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
 
     return result;   
 }
