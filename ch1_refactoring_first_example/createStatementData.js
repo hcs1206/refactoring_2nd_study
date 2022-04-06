8713dd941102397da655e9f2bca18d000b7bd67c
changseop_hwang@tmax.co.kr <changseop_hwang@tmax.co.kr>
2022-04-06 15:38:20 +0900

    1.8 다형성을 활용해 계산 코드 재구성하기 공연료 계산기를 다형성 버전으로 만들기(팩터리 함수, 서브클래스, 조건부 로직을 다형성으로 바꾸기)

--- ch1_refactoring_first_example/createStatementData.js
+++ ch1_refactoring_first_example/createStatementData.js
@@ -4,35 +4,47 @@ class PerformanceCalculator{
         this.play = aPlay;
     }
 
-    get amount(){ // amountFor() 함수의 코드를 계산기 클래스로 복사
-        let result = 0;
-    
-        switch(this.play.type){ // amountFor() 함수에서 매개변수로 받던 정보를 계산기 필드에서 바로 얻음
-        case "tragedy":
-            result = 40000;
-            if(this.performance.audience > 30){
-                result += 1000 * (this.performance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000;
-            if(this.performance.audience > 20){
-                result += 10000 + 500*(this.performance.audience - 20);
-            }    
-            result += 300*this.performance.audience;
-            break;
-        default:
-            throw new Error(`알 수 없는 장르: ${this.play.type}`);
+    get amount(){
+        throw new Error(`서브클래스에서 처리하도록 설계`);    
+    }
+
+    get volumeCredits(){
+        return Math.max(this.performance.audience - 30, 0);
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator{
+    get amount(){
+        let result = 40000;
+        if(this.performance.audience > 30){
+            result += 1000 * (this.performance.audience - 30);
         }
-    
+        return result;
+    }
+}
+
+class ComedyCalculator extends PerformanceCalculator{
+    get amount(){
+        let result = 30000;
+        if(this.performance.audience > 20){
+            result += 10000 + 500*(this.performance.audience - 20);
+        }    
+        result += 300*this.performance.audience;
         return result;
     }
 
     get volumeCredits(){
-        let result = 0;
-        result += Math.max(this.performance.audience - 30, 0);
-        if("comedy" == this.play.type) result += Math.floor(this.performance.audience / 5);
-        return result;
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
+    }
+}
+
+
+function createPerformanceCalculator(aPerformance, aPlay){
+    switch(aPlay.type){
+    case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy" : return new ComedyCalculator(aPerformance, aPlay);
+    default:
+        throw new Error(`알 수 없는 장르: ${aPlay.type}`);
     }
 }
 
@@ -45,7 +57,7 @@ function createStatementData(invoice, plays){
     return statementData;
 
     function enrichPerformance(aPerformance){
-        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance)); // 공연료 계산기 생성
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance)); // 생성자 대신 팩터리 함수 이용
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
         result.amount = calculator.amount;
