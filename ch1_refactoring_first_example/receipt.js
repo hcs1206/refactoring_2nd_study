9cc339531cfd2a974264f559464bc16402673733
changseop_hwang@tmax.co.kr <changseop_hwang@tmax.co.kr>
2022-04-06 13:55:43 +0900

    1.4 statement() 함수 분리 switch 문 추출

--- ch1_refactoring_first_example/receipt.js
+++ ch1_refactoring_first_example/receipt.js
@@ -4,33 +4,15 @@ function statement(invoice, plays){
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
     const format = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2}).format;
 
-    for(let performence of invoice.performances){
-        const play = plays[performence.playID];
-        let thisAmount = 0;
-
-        switch(play.type){
-        case "tragedy":
-            thisAmount = 40000;
-            if(performence.audience > 30){
-                thisAmount += 1000 * (performence.audience - 30);
-            }
-            break;
-        case "comedy":
-            thisAmount = 30000;
-            if(performence.audience > 20){
-                thisAmount += 10000 + 500*(performence.audience - 20);
-            }    
-            thisAmount += 300*performence.audience;
-            break;
-        default:
-            throw new Error(`알 수 없는 장르: ${play.type}`);
-        }
+    for(let performance of invoice.performances){
+        const play = plays[performance.playID];
+        let thisAmount = amountFor(performance, play);
         // 포인트 적립
-        volumeCredits += Math.max(performence.audience - 30, 0);
+        volumeCredits += Math.max(performance.audience - 30, 0);
         // 희극 관객 5명마다 추가 포인트 제공
-        if("comedy" == play.type) volumeCredits += Math.floor(performence.audience / 5);
+        if("comedy" == play.type) volumeCredits += Math.floor(performance.audience / 5);
 
-        result +=` ${play.name}: ${format(thisAmount/100)} (${performence.audience}석)\n`;
+        result +=` ${play.name}: ${format(thisAmount/100)} (${performance.audience}석)\n`;
         totalAmount += thisAmount;
     }
         // 청구 내역을 출력한다
@@ -38,6 +20,30 @@ function statement(invoice, plays){
         result += `적립 포인트: ${volumeCredits}점\n`;
 
         return result;
+}
+
+function amountFor(aPerformance, play){
+    let result = 0;
+
+    switch(play.type){
+    case "tragedy":
+        result = 40000;
+        if(aPerformance.audience > 30){
+            result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+    case "comedy":
+        result = 30000;
+        if(aPerformance.audience > 20){
+            result += 10000 + 500*(aPerformance.audience - 20);
+        }    
+        result += 300*aPerformance.audience;
+        break;
+    default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+
+    return result;
 }
 
 module.exports = statement;
