caf1288970024300a3c10c5fa6f1cac3fdc095d1
changseop_hwang@tmax.co.kr <changseop_hwang@tmax.co.kr>
2022-04-06 15:42:22 +0900

    1.9 예제 최종 코드

--- ch1_refactoring_first_example/createStatementData.js
+++ ch1_refactoring_first_example/createStatementData.js
@@ -1,3 +1,45 @@
+function createStatementData(invoice, plays){
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+    return statementData;
+
+    function enrichPerformance(aPerformance){
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance)); // 생성자 대신 팩터리 함수 이용
+        const result = Object.assign({}, aPerformance);
+        result.play = calculator.play;
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
+        return result;
+    }
+
+    function playFor(aPerformance){
+        return plays[aPerformance.playID];
+    }
+    
+    function totalAmount(data){
+        return data.performances
+        .reduce((total,aPerformance) => total + aPerformance.amount, 0);
+    }
+
+    function totalVolumeCredits(data){
+        return data.performances
+        .reduce((total, aPerformance) => total + aPerformance.volumeCredits, 0);
+    }
+
+}
+
+function createPerformanceCalculator(aPerformance, aPlay){
+    switch(aPlay.type){
+    case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy" : return new ComedyCalculator(aPerformance, aPlay);
+    default:
+        throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+    }
+}
+
 class PerformanceCalculator{
     constructor(aPerformance, aPlay){
         this.performance = aPerformance;
@@ -36,49 +78,6 @@ class ComedyCalculator extends PerformanceCalculator{
     get volumeCredits(){
         return super.volumeCredits + Math.floor(this.performance.audience / 5);
     }
-}
-
-
-function createPerformanceCalculator(aPerformance, aPlay){
-    switch(aPlay.type){
-    case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
-    case "comedy" : return new ComedyCalculator(aPerformance, aPlay);
-    default:
-        throw new Error(`알 수 없는 장르: ${aPlay.type}`);
-    }
-}
-
-function createStatementData(invoice, plays){
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return statementData;
-
-    function enrichPerformance(aPerformance){
-        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance)); // 생성자 대신 팩터리 함수 이용
-        const result = Object.assign({}, aPerformance);
-        result.play = calculator.play;
-        result.amount = calculator.amount;
-        result.volumeCredits = calculator.volumeCredits;
-        return result;
-    }
-
-    function playFor(aPerformance){
-        return plays[aPerformance.playID];
-    }
-    
-    function totalAmount(data){
-        return data.performances
-        .reduce((total,aPerformance) => total + aPerformance.amount, 0);
-    }
-
-    function totalVolumeCredits(data){
-        return data.performances
-        .reduce((total, aPerformance) => total + aPerformance.volumeCredits, 0);
-    }
-
 }
 
 module.exports = createStatementData;
